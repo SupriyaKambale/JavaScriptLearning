a6b188b673f952028dbeab060597020b5699dc6b
SupriyaKambale <kambalesupriya0369@gmail.com>
2022-12-13 10:10:48 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,13 @@
+var myFullName;
+console.log(myFullName);
+
+
+myFullName="Supriya Kambale";
+console.log(myFullName);
+
+
+myFullName="Supriya Tanaji Kambale";
+console.log(myFullName);
+
+var myLove="I love only JavaScript";
+console.log(myLove);
