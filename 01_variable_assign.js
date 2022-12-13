d19882ce42ab142da9a80401ac7459305211c07b
SupriyaKambale <kambalesupriya0369@gmail.com>
2022-12-13 10:35:24 +0530

    Update 01_variable_assign.js

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -1,3 +1,4 @@
+console.log("\n -------Q1.-------"); 
 var myFullName;
 console.log(myFullName);
 
@@ -9,5 +10,17 @@ console.log(myFullName);
 myFullName="Supriya Tanaji Kambale";
 console.log(myFullName);
 
+console.log("\n -------Q2.-------"); 
 var myLove="I love only JavaScript";
 console.log(myLove);
+
+console.log("\n -------Q3 -------");
+var firstName="supriya";
+var lastName="kambale";
+var age="32";
+var address="Pune";
+var pin_code="411033";
+var gender="female";
+var is_married="yes";
+console.log(firstName, lastName, age, address, pin_code, gender, is_married);
+
