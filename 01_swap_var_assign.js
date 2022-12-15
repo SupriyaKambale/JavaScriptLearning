b6f915a7031a258c0a116401b82426fc7b4bbfbc
SupriyaKambale <kambalesupriya0369@gmail.com>
2022-12-15 09:49:04 +0530

    Create 01_swap_var_assign.js

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,43 @@
+console.log("-------Practice----------");
+var myNumber, yourNumber, temp;
+myNumber=200;
+yourNumber=300;
+console.log("------Before swapping------");
+console.log("myNumber=",myNumber);
+console.log("yourNumber=",yourNumber);
+temp=myNumber;
+myNumber=yourNumber;
+yourNumber=temp;
+console.log("------After swapping------");
+console.log("myNumber=",myNumber);
+console.log("yourNumber=",yourNumber);
+
+console.log("\n-------Q1----------");
+var sweety,cutie, temp;
+sweety="Sweety";
+cutie="Cutie";
+console.log("------Before swapping------");
+console.log("sweety=",sweety);
+console.log("cutie=",cutie);
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("\n------After swapping------");
+console.log("sweety=",sweety);
+console.log("cutie=",cutie);
+
+console.log("\n\n-------Q2----------");
+var num1=100, num2=200, num3=300, temp;
+console.log("------Before swapping------");
+console.log("num1=",num1, "num2=",num2, "num3=",num3);
+console.log("------After swapping------");
+temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("num1=",num1, "num2=",num2, "num3=",num3);
+
+
+
+
+
