e2f76810b22ce4b42d82b03f703c4da8e17b8530
SupriyaKambale <kambalesupriya0369@gmail.com>
2023-01-07 10:09:03 +0530

    Create 01_typeOfAssignment.js

---
+++ 01_typeOfAssignment.js
@@ -0,0 +1,29 @@
+console.log("\n Q1. Print data type along with before and after initialization.");
+var bankName;
+console.log("Datatype BEFORE Initialization:", typeof bankName);
+bankName="SBI Bank";
+console.log("Datatype AFTER Initialization:", typeof bankName);
+
+
+
+console.log("\n Q2. Name marks to variable with value 90%");
+var marks="90%";
+console.log("Datatype of marks:", typeof marks);
+var marks=90;
+console.log("Datatype of marks:", typeof marks);
+
+
+console.log("\n Q3. isWorking with true value and print datatype");
+var isWorking;
+console.log("isWorking datatype BEFORE:", typeof isWorking);
+var isWorking=true;
+console.log("isWorking datatype AFTER:", typeof isWorking);
+
+console.log("\n Q4.A. Declare variable totalCount with 'Hundred and seven' and log on the console.");
+var totalCount="Hundred And Seven";
+console.log("totalCount value:", totalCount, "\n datatype:", typeof totalCount);
+
+
+console.log("\n Q4.B. Update total Count variable and print dataType");
+var totalCount=107;
+console.log("totalCount value:", totalCount, "\n datatype:", typeof totalCount);
